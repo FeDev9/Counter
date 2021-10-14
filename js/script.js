bbd28e5a2785fbfc0703b3ed8bde3ba0c07293f9
federicocecconi <fedececco9.github@manyme.com>
2021-10-14 18:19:32 +0200

    init commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let counterGrid = document.createElement('div');
+document.body.prepend(counterGrid);
+counterGrid.setAttribute('class', 'counter-grid'
+);
+let counterPlus = document.createElement('button');
+counterGrid.prepend(counterPlus);
+counterPlus.textContent = "+";
+counterPlus.style.backgroundColor = 'blue';
+
+let counterBox = document.createElement('div');
+counterGrid.prepend(counterBox);
+counterBox.setAttribute('class', 'counter-box');
+counterBox.textContent = 0;
+
+let counterMinus= document.createElement('button');
+counterGrid.prepend(counterMinus);
+counterMinus.textContent = "-";
+counterMinus.style.backgroundColor = 'red';
+
+counterPlus.addEventListener('click', function() {
+    counterBox.textContent = Number(counterBox.textContent)+1;
+})
+
+counterMinus.addEventListener('click', function() {
+    counterBox.textContent = Number(counterBox.textContent)-1;
+})
+
