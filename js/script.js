a79d4bab64d72da7b446ad8d2bd5d7cf6c9180ea
federicocecconi <fedececco9.github@manyme.com>
2021-10-15 09:39:11 +0200

    style commit

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ counterGrid.setAttribute('class', 'counter-grid'
 let counterPlus = document.createElement('button');
 counterGrid.prepend(counterPlus);
 counterPlus.textContent = "+";
-counterPlus.style.backgroundColor = 'blue';
+counterPlus.style.backgroundColor = '#627bf5';
 
 let counterBox = document.createElement('div');
 counterGrid.prepend(counterBox);
@@ -15,7 +15,7 @@ counterBox.textContent = 0;
 let counterMinus= document.createElement('button');
 counterGrid.prepend(counterMinus);
 counterMinus.textContent = "-";
-counterMinus.style.backgroundColor = 'red';
+counterMinus.style.backgroundColor = '#ff9797';
 
 counterPlus.addEventListener('click', function() {
     counterBox.textContent = Number(counterBox.textContent)+1;
@@ -23,4 +23,29 @@ counterPlus.addEventListener('click', function() {
 
 counterMinus.addEventListener('click', function() {
     counterBox.textContent = Number(counterBox.textContent)-1;
+})
+
+counterMinus.addEventListener('mouseenter', function(){
+    if(!counterMinus.classList.contains('hover'))
+        counterMinus.classList.add('hover');
+        counterMinus.style.backgroundColor='red';
+})
+
+counterMinus.addEventListener('mouseleave', function(){
+    if(counterMinus.classList.contains('hover'))
+        counterMinus.classList.remove('hover');
+        counterMinus.style.backgroundColor='#ff9797';
+})
+
+counterPlus.addEventListener('mouseenter', function(){
+    if(!counterPlus.classList.contains('hover'))
+        counterPlus.classList.add('hover');
+        counterPlus.style.backgroundColor='blue';
+})
+
+counterPlus.addEventListener('mouseleave', function(){
+    if(counterPlus.classList.contains('hover'))
+        counterPlus.classList.remove('hover');
+        counterPlus.style.backgroundColor='#627bf5';
+        
 })
